103f10f53b7e59cc7cf2428d942e766b46f2fbd5
cf20orianarojas <rojasoriana260217@gmail.com>
2024-02-15 18:59:45 +0100

    drop-area events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-console.log('Hello there')
+/*  */
+let files = [];
+
+const dropArea = document.querySelector('.drop-area');
+const dragDropText = document.querySelector('h2');
+const button = document.querySelector('button');
+const input = document.querySelector('#input-file');
+const preview = document.querySelector('#preview');
+
+let evt = ['dragover', 'dragleave', 'drop'];
+
+evt.forEach(event => {
+    dropArea.addEventListener(event, prevDefault);
+});
+
+function prevDefault(e) {
+    e.preventDefault();
+}
+
+dropArea.addEventListener("dragover", () => {
+    dropArea.classList.add('active');
+    dropArea.innerText='In';
+});
+
+dropArea.addEventListener("dragleave", () => {
+    dropArea.classList.remove('active');
+    dropArea.innerText='Out';
+});
+
+dropArea.addEventListener("drop", (event)=>{});
+dropArea.addEventListener("drop", function(e){});
