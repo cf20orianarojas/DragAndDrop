102f3485b0b1fe96476c64684d9840cc7238a358
cf20orianarojas <rojasoriana260217@gmail.com>
2024-02-17 18:20:58 +0100

    WIP - drag and drop

--- js/index.js
+++ js/index.js
@@ -7,25 +7,41 @@ const button = document.querySelector('button');
 const input = document.querySelector('#input-file');
 const preview = document.querySelector('#preview');
 
-let evt = ['dragover', 'dragleave', 'drop'];
-
-evt.forEach(event => {
-    dropArea.addEventListener(event, prevDefault);
-});
+let evt = ["dragover", "dragleave", "drop"];
 
 function prevDefault(e) {
     e.preventDefault();
 }
 
+evt.forEach(event => {
+    dropArea.addEventListener(event, prevDefault);
+});
+
 dropArea.addEventListener("dragover", () => {
     dropArea.classList.add('active');
-    dropArea.innerText='In';
+    dragDropText.textContent='Drop to upload files';
 });
 
 dropArea.addEventListener("dragleave", () => {
     dropArea.classList.remove('active');
-    dropArea.innerText='Out';
+    dragDropText.textContent='Drag & Drop files';
 });
 
-dropArea.addEventListener("drop", (event)=>{});
-dropArea.addEventListener("drop", function(e){});
+dropArea.addEventListener("drop", (event) => {
+    dropArea.classList.remove('active');
+    files = files.concat(Array.from(event.dataTransfer.files));
+    dragDropText.textContent='Drag & Drop files';
+    showFiles();
+});
+
+function showFiles() {
+    if (files.length === undefined) return
+    files.forEach((file, index) => {
+        processFile(file, index);
+        console.log(file)
+    });
+}
+
+function processFile(file, index) {
+    const validExtensions = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'];
+}
