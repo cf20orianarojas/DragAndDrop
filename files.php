f9498226fb4b3d31c3935247aabd0a0ca0449eef
cf20orianarojas <rojasoriana260217@gmail.com>
2024-02-19 19:47:31 +0100

    file php

--- files.php
+++ files.php
@@ -1,10 +1,21 @@
-<?php 
-    print_r($_FILES["inputFiles[]"]);
-    // if (isset($_FILES["inputFiles"])) {
-
-    //     for ($i = 0; $i < count($_FILES["inputFiles"]["name"]); $i++) {
-    //         echo $_FILES["inputFiles"]["tmp_name"][$i] . '<br>';  
-    //         echo $_FILES["inputFiles"]["name"][$i] . '<br>';  
-    //     }
-    // }
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP files</title>
+</head>
+<body>
+    <?php 
+        echo "Hola caracola";
+         if (isset($_FILES["inputFiles[]"])) {
+            print_r($_FILES["inputFiles[]"]);
+        }
+        //     for ($i = 0; $i < count($_FILES["inputFiles"]["name"]); $i++) {
+        //         echo $_FILES["inputFiles"]["tmp_name"][$i] . '<br>';  
+        //         echo $_FILES["inputFiles"]["name"][$i] . '<br>';  
+        //     }
+        // }
+    ?>
+</body>
+</html>
